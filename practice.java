762532e343288ed82c58a598377576871da247bf
BatTwrite <tamilwired@gmail.com>
2025-11-26 22:34:32 +0530

    test

---
+++ practice.java
@@ -0,0 +1,18 @@
+public class practice {
+    public static void main(String[] args){
+
+        //find the highest number !
+                 int a= args.length >0 ?10 : 0;
+                 int b=5;
+                 int c=15;
+
+                 if(a>b && a>c){
+                        System.out.println(a);
+                 }else if(b>c){
+                        System.out.println(b);
+                 }else{
+                        System.out.println(c);
+                 }
+
+    }
+}
