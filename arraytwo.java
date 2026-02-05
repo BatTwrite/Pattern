f63a0795f223d1f19528aebc3ade558160c07c32
BatTwrite <tamilwired@gmail.com>
2026-02-05 06:50:18 +0530

    new sample changes

--- arraytwo.java
+++ arraytwo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class arraytwo {
     
     public static void main(String[] args) {
@@ -25,5 +27,89 @@ public class arraytwo {
             }
             
         }
+        
+        // Filter covid cases based on the country name,Total cases,RecoveryCases,Active cases.
+        
+        System.out.println("Filter covid case Data ");
+        System.out.println("1. Name");
+        System.out.println("2. Total Cases");
+        System.out.println("3. Recoverd Cases");
+        System.out.println("4. Active Cases");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your choise");
+        int choise = scanner.nextInt();
+
+
+        switch (choise) {
+            case 1:
+                scanner.nextLine();
+                System.out.println("Enter name of the country");
+                String countryName = scanner.nextLine();
+                  int idx = -1;
+                  for(int i =0;i<Name.length;i++){
+                    if(countryName.equalsIgnoreCase(Name[i])){
+                        idx=i;
+                        break;
+                    }
+                  }
+                  if(idx==-1){
+                    System.out.println("Sorry Country Not Found"); 
+                  }else{
+                    for(int element : covidCases[idx]){
+                        System.out.println(element+"\t");
+                    }
+                    System.out.println();
+                  }
+
+                break;
+            case 2:
+                System.out.println("Total Cases ");
+                System.out.println(".......................");
+                for(int i =0; i<covidCases.length;i++){
+            System.out.println(Name[i]);
+            System.out.println("--------------");
+                System.out.print(covidCases[i][totalcases]+"\t");
+               
+
+                System.out.println();
+            }
+                
+                break;
+            case 3:
+            System.out.println("Total Recovered Cases");
+                System.out.println(".......................");
+                for(int i =0; i<covidCases.length;i++){
+            System.out.println(Name[i]);
+            System.out.println("--------------");
+                System.out.print(covidCases[i][totalrecoverycases]+"\t");
+               
+                
+                System.out.println();
+            }    
+            
+                break;
+            case 4:
+                System.out.println("Total Active Cases ");
+                System.out.println(".......................");
+                for(int i =0; i<covidCases.length;i++){
+            System.out.println(Name[i]);
+            System.out.println("--------------");
+                System.out.print(covidCases[i][activecases]+"\t");
+               
+                
+                System.out.println();
+            }
+                
+                break;        
+            default:
+                break;
+        }
+        scanner.close();
+
+
+
+
+
     }
 }
