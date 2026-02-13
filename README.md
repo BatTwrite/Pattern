2886c71f16e07d76f8273e70dd57d1957d9ceab0
BatTwrite <tamilwired@gmail.com>
2026-02-13 05:49:47 +0530

    insertion short

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 java patterns exampls
 
 In this pattern file had a 23 basic patterns with java programme!
+
+i have intiatd the Insertion short code  in math file ..
--- math.java
+++ math.java
@@ -73,8 +73,27 @@ return 0;
  //PalindRome(1331);
  //AmStrong number
  //Amstrong(153);
- Prime(6);
- 
+ // Prime(6);
+
+ //insertion shorting 
+
+ int [] a= {8,8,7,6,5,4,3,2,1};
+ int i=0;
+ int key=0;
+ for(int j = 1;j<a.length;j++){
+    key= a[j];
+
+    i=j-1;
+
+    while(i>0&&a[i]>key){
+        a[i+1]=a[i];
+         i=i-1;
+    }
+    a[i+1]=key;
+ }
+     for( int vale : a){
+        System.out.println(vale);
+     }
 
     }
 }
